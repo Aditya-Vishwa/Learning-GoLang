d92f2bbfc104b1597912f1bfa24168b8cfd7cd81
Aditya Kumar Sharma <vishwaaditya2003@gmail.com>
2023-04-06 05:16:18 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main // importing main package
+
+import "fmt" //importing fmt or format package
+
+func main(){ //main function
+	fmt.Print("Hello User\t") // printing
+	fmt.Println("I am Aditya Sharma")
+	fmt.Println("A Computer Science Student")
+}
