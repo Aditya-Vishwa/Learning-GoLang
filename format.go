93b6ce792594203967bca27303d4997d424b15f1
Aditya Kumar Sharma <vishwaaditya2003@gmail.com>
2023-04-07 03:27:54 +0530

    Format Printing By Printf

---
+++ format.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	var v1 = "I am Aditya"
+	const c1 = "A Computer Science Student"
+	fmt.Printf("Hey Folks!\n%s\n",v1)
+	fmt.Printf("And I am %s in LNCT University",c1)
+}
