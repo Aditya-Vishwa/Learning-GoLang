bb87a895880f9228af1778d554b3aa4d7bd1462d
Aditya Kumar Sharma <vishwaaditya2003@gmail.com>
2023-04-08 11:15:32 +0530

    Datatypes in GO

--- datatype.go
+++ datatype.go
@@ -7,5 +7,8 @@ func main(){
 	a = 20
 	var b string
 	b = "Aditya"
-	fmt.Printf("I am %v and I am %v years old",b,a)
+	var c float32
+	c=1.98
+	fmt.Printf("I am %v and I am %v years old\n",b,a)
+	fmt.Printf("%v is a float datatype",c)
 }
