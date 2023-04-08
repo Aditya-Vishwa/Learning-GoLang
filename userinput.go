efc89a40dd71cacb2f0e1c74737669eb4f8e57f8
Aditya Kumar Sharma <vishwaaditya2003@gmail.com>
2023-04-08 12:30:14 +0530

    User Input & Pointers

---
+++ userinput.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	var firstname string
+	fmt.Printf("Enter your first Name: ")
+	fmt.Scan(&firstname) // Getting input
+	var lastname string
+	fmt.Printf("Enter your last Name: ")
+	fmt.Scan(&lastname) // & is refered here a pointer
+	var age int
+	fmt.Printf("Enter your age: ")
+	fmt.Scan(&age) // In Go pointer is special type of varibale
+
+}
