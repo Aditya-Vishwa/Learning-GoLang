40ccbf288c7b04c0e9212b113cafcf0ef7206040
Aditya Kumar Sharma <vishwaaditya2003@gmail.com>
2023-04-08 13:26:03 +0530

    Datatypes in Go

--- datatype.go
+++ datatype.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main(){
-	var a int
+	var a int // integer datatype
 	a = 20
-	var b string
+	var b string // string datatype
 	b = "Aditya"
-	var c float32
+	var c float32 // float datatype
 	c=1.98
 	fmt.Printf("I am %v and I am %v years old\n",b,a)
 	fmt.Printf("%v is a float datatype",c)
